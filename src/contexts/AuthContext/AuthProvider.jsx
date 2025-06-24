2be6ddd024720ed447996dbdc9203cf184de505d
a1shuvo <a1shuvosaha@gmail.com>
2025-06-24 21:57:14 +0600

    Created firebase project setup and auth context structure

---
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -0,0 +1,8 @@
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const authinfo = {};
+  return <AuthContext value={authinfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import AuthProvider from "./contexts/AuthContext/AuthProvider";
 import "./index.css";
 import { router } from "./router/router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="max-w-7xl mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>
 );
